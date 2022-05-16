47936bdae6f8506e85caceff63d6c87344d4e0f3
Ruddytip <anton.rudakov216@mail.ru>
2022-05-16 15:37:57 +0300

    update

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "matrix.hpp"
+
+int main(){
+	// Matrix m = Matrix(2, 3);
+	// m[0][0] = 234.5;
+	// Matrix a = std::vector<std::vector<double>>{
+	// 			{3, 1, 5},
+	// 			{4, 1, 5}
+	// 		};
+	// Matrix b = std::vector<std::vector<double>>{
+	// 			{5, 4},
+	// 			{2, 1},
+	// 			{1, 3}
+	// };
+	// Matrix m = a.transpose();
+	// std::cout << a;
+	// std::cout << m;
+	return 0;
+}
